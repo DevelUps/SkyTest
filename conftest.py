5228a214d59c47839f9fa29bd36b4526aa956b57
Pierre <grandett1@gmail.com>
2025-03-15 21:57:58 -0500

    Fix conftest

--- conftest.py
+++ conftest.py
@@ -1,22 +1,9 @@
 import pytest
-from playwright.sync_api import sync_playwright
-
-def pytest_addoption(parser):
-    """ Agrega la opción --browser en pytest """
-    parser.addoption("--browser", action="store", default="chromium", help="Selecciona el navegador: chromium, firefox o webkit")
 
 @pytest.fixture(scope="session")
-def browser_type(pytestconfig):
-    """ Configura el navegador basado en la opción pasada en pytest """
-    browser_name = pytestconfig.getoption("browser")
-    
-    with sync_playwright() as p:
-        if browser_name == "firefox":
-            browser = p.firefox.launch(headless=True)
-        elif browser_name == "webkit":
-            browser = p.webkit.launch(headless=True)
-        else:
-            browser = p.chromium.launch(headless=True)
-        
-        yield browser
-        browser.close()
+def browser_context(browser):
+    """ Devuelve un contexto del navegador usando el fixture integrado de Playwright """
+    context = browser.new_context()
+    yield context
+    context.close()
+
