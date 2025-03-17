8e097bf6738cf1050eda19118c96c696298340df
Pierre <grandett1@gmail.com>
2025-03-17 09:35:51 -0500

    Forzar ejecución de GitHub Actions

--- tests/test_login.py
+++ tests/test_login.py
@@ -5,6 +5,6 @@ def test_valid_login(page):
     login_page = LoginPage(page)
 
     login_page.navigate()
-    login_page.login(DEFAULT_USERNAME, DEFAULT_PASSWORD)
+    login_page.login(DEFAULT_xSERNAME, DEFAULT_PASSWORD)
 
     assert login_page.is_logged_in(), "El login no fue exitoso"
